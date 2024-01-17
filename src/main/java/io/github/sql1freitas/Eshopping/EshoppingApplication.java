e900f86b0dcd987dc5f616d94ce07abf7ba4ec2c
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-16 21:20:32 -0300

    Initial commit

---
+++ src/main/java/io/github/sql1freitas/Eshopping/EshoppingApplication.java
@@ -0,0 +1,13 @@
+package io.github.sql1freitas.Eshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sql1freitas/Eshopping/EshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sql1freitas.Eshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
